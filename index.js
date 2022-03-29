cb604e12334424dc360d97380f80fbbf6aadbbe9
tun35770 <tun35770@temple.edu>
2022-03-29 19:48:03 -0400

    Academic Reminders (assigning & reminding)

--- index.js
+++ index.js
@@ -27,6 +27,13 @@ client.on('message' , async message => {
             
         }
 
+        if (command.toLowerCase() == 'assign'){
+            assign(splitMessage, message);
+        }
+
+        if(command.toLowerCase() == 'remind'){
+            remind(message);
+        }
     }
 })
 
@@ -75,7 +82,45 @@ function poll(splitMessage, message){
             }          
 }
 
+//array to hold assignments added via assign command
+var assignments = [];
 
+//Add assignment to reminders list
+function assign(splitMessage, message){
+    
+    const assignEmbed = new MessageEmbed()
+            .setTitle("How to add assignment")
+            .setDescription("Use $tu assign to add an assignment\n" +
+            "**Example: $tu assign Quiz 2 3/29**\n\n")
+
+    if(!splitMessage[2]){   //no assignment given
+        message.channel.send({ embeds: [assignEmbed] });
+        return;
+    }
+
+    let assignment = splitMessage.slice(2).join(" ");
+    assignments.push(assignment);
+    message.channel.send("Assignment successfully added")
+}
+
+//Academic Reminder
+function remind(message){
+
+    if(assignments.length < 1)
+        return;
+
+    let allAssignments = "";
+    
+    for(let i in assignments){      //build description of embed
+        allAssignments += '- ' + assignments[i] + '\n';
+    }
+
+    const remindEmbed = new MessageEmbed()
+            .setTitle("Assignments Due")
+            .setDescription(allAssignments);
+
+    message.channel.send({ embeds: [remindEmbed] });
+}
 
 client.login(process.env.DISCORD_BOT_TOKEN)
 
