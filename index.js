13bee08081b798f85560591fce217b92ca33e0e9
erinhb <tui90880@temple.edu>
2022-03-29 20:44:52 -0400

    refactored poll function to new file

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const { Client , Intents, MessageEmbed, MessageReaction } = require('discord.js')
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
+const poll = require("./poll.js");
+
 client.on('ready' , () => {
     console.log('TU Bot is online!')
 })
@@ -22,9 +24,12 @@ client.on('message' , async message => {
             await message.reply("Goodbye!")
         }
 
+        if(command.toLowerCase() == 'help'){
+            await message.reply("Commands: $tu hello, $tu bye, $tu poll, $tu assign, $tu remind")
+        }
+
         if (command.toLowerCase() == 'poll'){
-            poll(splitMessage, message);
-            
+            poll.poll(splitMessage, message);
         }
 
         if (command.toLowerCase() == 'assign'){
@@ -36,51 +41,6 @@ client.on('message' , async message => {
         }
     }
 })
-
-//Poll function
-function poll(splitMessage, message){
-    const nums = ["1️⃣","2️⃣","3️⃣","4️⃣","5️⃣","6️⃣","7️⃣","8️⃣","9️⃣"];
-    //Error message if too few arguments ar given
-    const pollEmbed = new MessageEmbed()
-                .setTitle("How to make a poll")
-                .setDescription("Ask a question for a yes or no poll\n" +
-                "**Example: $tu poll Should we have class today?**\n\n" +
-                "Or Put arguments at the end of the question seperated by a + for multiple choice poll\n"+
-                "**Example: $tu poll Best Programming Language? + Java + Python + C**");
-            if(!splitMessage[2]){//Check arguments
-                message.channel.send({ embeds: [pollEmbed] }); 
-            }else{
-                //Join the poll back to gether
-                let pollArgs = splitMessage.slice(2).join(" ");
-                //Split up the choices
-                let choices = pollArgs.split('+');
-                if(!choices[1]){ //If no choices, make it a yes or no poll
-                    message.channel.send("📋**" + pollArgs + "**\n").then(messageReaction => {
-                        messageReaction.react("👍");
-                        messageReaction.react("👎");
-                        //Delete message after poll is made
-                        message.delete(5000);
-                    });
-                    
-                } else if (!choices[11]){ //If there are choice (less than 11), give a reaction to each choice
-                    var pollReply = "";
-                    //Set the title of the poll
-                    pollReply += "📋**" + choices[0] + "**\n";
-                    //Loop through the options and assign a reaction to each
-                    for(var i = 1; i<choices.length;i++){
-                        pollReply += nums[i-1] + ": " + choices[i] + "\n";
-                    }
-                    //Then reply with all the reactions so users can see
-                    message.channel.send(pollReply).then(messageReaction => {
-                        for(var i = 1; i<choices.length;i++){
-                            messageReaction.react(nums[i-1]);
-                        }
-                        //Delete message after poll is made
-                        message.delete(5000);
-                    });
-                }  
-            }          
-}
 
 //array to hold assignments added via assign command
 var assignments = [];
