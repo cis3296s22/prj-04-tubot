6a51db0241484b76aafa83d72ce9d3ea18d0e1d1
Thomas Toal <tuj55883@temple.edu>
2022-04-11 19:06:55 -0400

    Fixed incorrect role command calls

--- roles.js
+++ roles.js
@@ -3,25 +3,45 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 
 //Creates role from users message
 function createRole(splitMessage, message) {
+    const createEmbed = new MessageEmbed()
+    .setTitle("How to create a role")
+    .setDescription("Type the new role name after the command\n" +
+        "**Example: $tu createRole UserRole**");
+    const {guild} = message;
     if(splitMessage[2] != null){
         let roleArgs = splitMessage.slice(2).join(" ");
-
-
-        message.guild.roles.create({
-            name: roleArgs,
+        roleName = roleArgs;
+        console.log(roleName)
+        const role = guild.roles.cache.find((role) =>{
+            return role.name === roleName
+        }
+        );
+        if(role){
+            message.reply(`There is already a "${roleName}" role `)
+        } else {
+            message.guild.roles.create({
+                name: roleArgs,
             
-        })
-        message.reply(`The "${roleArgs}" role has been created`)
+            })
+            message.reply(`The "${roleArgs}" role has been created`)
+        }
     } else {
-        message.reply("Please also type the name of the new role after createRole")
+        message.channel.send({ embeds: [createEmbed] });
     }
-
 }
 //Gives a role to a user
 function giveRole(splitMessage, message) {
+    const giveEmbed = new MessageEmbed()
+    .setTitle("How to give a user a role")
+    .setDescription("Type @ user name and the role you want to give\n" +
+        "**Example: $tu giveRole @user UserRole**");
     const target = message.mentions.users.first();
     const {guild} = message;
-    if(splitMessage[3] != null){
+    if(!target){
+        message.reply(`Make sure you use @ to specify the user`)
+    } else if(splitMessage[4] != null) {
+        message.reply("Too many arguments")
+    } else if(splitMessage[3] != null){
         let roleArgs = splitMessage.slice(2).join(" ");
         let splitRoleArgs = roleArgs.split(" ");
         const person = guild.members.cache.get(target.id);
@@ -32,16 +52,20 @@ function giveRole(splitMessage, message) {
         }
         );
         if(!role){
-            message.reply(`There is no"${roleName}" role `)
+            message.reply(`There is no "${roleName}" role `)
+        } else if(!person){
+            message.reply(`Make sure you use @ to specify the user`)
         } else{
             person.roles.add(role);
             message.reply(`User was given "${roleName}" role `)
         }
       
         
-    } else {
-        message.reply("Please also type the name of the new role and the persons name after giveRole")
-
+    } else if(splitMessage[2] != null) {
+        message.reply("Please specify role")
+    }else {
+        
+        message.channel.send({ embeds: [giveEmbed] });
     }
 
 }
@@ -49,9 +73,18 @@ function giveRole(splitMessage, message) {
 
 //Removes a role from a user
 function removeRole(splitMessage, message) {
+    const removeEmbed = new MessageEmbed()
+    .setTitle("How to revoke a user's role")
+    .setDescription("Type @ user name and the role you want to revoke\n" +
+        "**Example: $tu removeRole @user UserRole**");
     const target = message.mentions.users.first();
+    
     const {guild} = message;
-    if(splitMessage[3] != null){
+    if(!target){
+        message.reply(`Make sure you use @ to specify the user`)
+    } else if(splitMessage[4] != null) {
+        message.reply("Too many arguments")
+    } else if(splitMessage[3] != null){
         let roleArgs = splitMessage.slice(2).join(" ");
         let splitRoleArgs = roleArgs.split(" ");
         const person = guild.members.cache.get(target.id);
@@ -62,17 +95,21 @@ function removeRole(splitMessage, message) {
         }
         );
         if(!role){
-            message.reply(`There is no"${roleName}" role `)
+            message.reply(`There is no "${roleName}" role `)
         } else{
-            person.roles.remove(role);
-            message.reply(`User has been revoked of "${roleName}" role`)
+            if(person.roles.remove(role)){
+                message.reply(`User has been revoked of "${roleName}" role`)
+            } else{
+                message.reply(`User does not have "${roleName}" role`)
+            }
         }
       
 
         
+    } else if(splitMessage[2] != null) {
+        message.reply("Please specify role")
     } else {
-        message.reply("Please also type the name of the new role and the persons name after giveRole")
-        message.reply(`User was given "${roleName}" role `)
+        message.channel.send({ embeds: [removeEmbed] });
     }
 
 }
